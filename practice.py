a45759425ba403652cb6c86454794ebec2a997aa
harishAderu <37216029+harishAderu@users.noreply.github.com>
2019-08-24 22:22:01 +0530

    garts

--- practice.py
+++ practice.py
@@ -6,7 +6,11 @@ x
 np.sin(x)
 #fig = plt.figure()
 plt.plot(x, np.sin(x))
-plt.savefig('plot2.png')
+plt.savefig('plot1.png')
 plt.savefig('D:/pyworks/plots/plot1.png')
 plt.show(); # import when running from script
 
+import matplotlib.image as mpimg
+img = mpimg.imread('plot1.png')
+print(img)
+imgplot = plt.imshow(img)
